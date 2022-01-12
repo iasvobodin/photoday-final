28210bcac4229fde9e327082cac86347ab2c180f
iasvobodin <iasvobodin@gmail.com>
2022-01-12 12:47:57 +0500

    update dep

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
-// import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
 	kit: {
 		adapter: adapter({
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: null,
+			precompress: true
 		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
